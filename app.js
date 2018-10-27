d7878d18a96964b2dc5681a35b0b2b8bf6a48c38
ahmad62596 <ahmad62596@gmail.com>
2018-10-26 18:01:11 -0700

    Commiting question 4 after working

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ function sum(a, b) { //eslint-disable-line
   return [a + b, 'The sum of ' + a + ' and ' + b + ' is ' + (a + b) + '.'];
 }
 
+
+
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
@@ -76,12 +78,19 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  var answer = 0;
+  for (var i = 0; i < testArray.length; i++) {
+    answer = sum(answer, testArray[i])[0];
+  }
+  return [answer, testArray[0]+ ',' + testArray[1] + ',' + testArray[2] + " was passed in as an array of numbers, and " + answer + ' is their sum.' ]
 }
+
+
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
